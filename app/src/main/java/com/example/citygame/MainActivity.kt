6644546e9c94d9e5b36dcc27db0f46c69b42f1a8
Arxangel.80 <Arxangel.80@gmail.com>
2024-04-26 23:55:36 +0200

    GoogleMaps prototype

--- app/src/main/java/com/example/citygame/MainActivity.kt
+++ app/src/main/java/com/example/citygame/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.citygame
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +10,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.location.FusedLocationProviderClient
 
 
 enum class QuestScreen() {
@@ -19,14 +22,18 @@ enum class QuestScreen() {
 }
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (!checkLocationPermission()) {
+            requestLocationPermission()
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             val navController: NavHostController = rememberNavController()
             NavHost(
                 navController = navController,
-                startDestination = QuestScreen.RLEQuesth.name
-//                startDestination = QuestScreen.Login.name
+                startDestination = QuestScreen.Main.name
             ) {
                 composable(route = QuestScreen.Main.name) {
                     MainScreen()
@@ -36,5 +43,23 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    private val PERMISSION_REQUEST_CODE = 123
+    private fun checkLocationPermission(): Boolean {
+        return (this.checkSelfPermission(
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                && this.checkSelfPermission(
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+    private fun requestLocationPermission() {
+        this.requestPermissions(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            PERMISSION_REQUEST_CODE
+        )
     }
 }
